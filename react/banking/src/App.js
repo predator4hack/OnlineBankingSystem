75fb3925c2b286f8c7704a24431038bfa4a8b91e
Kumar <Kumar.Shubham@wellsfargo.com>
2023-08-10 15:08:29 +0530

    Added user register form

--- react/banking/src/App.js
+++ react/banking/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddCustomer from './Components/addCustomer';
+import React, { Component }  from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddCustomer/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
